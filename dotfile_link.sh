6b64f970eb9bb050456d6819db73eda136bd4527
kevin.justice <kevin.justice@prometheanworld.com>
2016-04-29 12:17:54 -0400

    switched from symbolic to hard links so the files have content instead of path info

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # list of all the tracked config files (consider tracking oh-my-zsh) 
-dotfiles=(.aliases .gitconfig .screenrc .sh-common .tmux.conf .vimrc .zshrc)
+dotfiles=(.aliases .gitconfig .screenrc .sh-common .tmux.conf .vimrc .zshrc .warprc)
 
 # creates links to all the tracked files
 for f in "${dotfiles[@]}"; do
     if [ -f "$f" ]; then
-        ln -s -i $HOME/"$f" "$f" 
+        ln -i $HOME/"$f" "$f" 
     fi
 done
 
